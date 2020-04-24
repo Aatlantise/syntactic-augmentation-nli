142a068e136b4bd3d545df618d0a6b67bef199ad
Junghyun Min <jmin10@jhu.edu>
2020-04-24 18:06:44 +0900

    Create hans_eval.sh

---
+++ config/hans_eval.sh
@@ -0,0 +1,16 @@
+export BERT_BASE_DIR=path/to/base/model/like/uncased_L-12_H-768_A-12
+export HANS_DIR=path/to/hans
+export TRAINED_CLASSIFIER=path/to/trained/classifier
+export OUTPUT_DIR=path/to/output/folder
+
+#Evaluate trained model on HANS
+
+python run_classifier.py \
+  --task_name=MNLI \
+  --do_predict=true \
+  --data_dir=$HANS_DIR \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$TRAINED=CLASSIFIER \
+  --max_seq_length=128 \
+  --output_dir=$OUTPUT_DIR
